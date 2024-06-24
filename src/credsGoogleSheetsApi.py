b418bca23e9aaacb172e0ab7c257d08ae133b893
kanyesteves <kany.esteves@gmail.com>
2024-06-24 08:26:05 -0300

    Criado conexão, e corrigindo problemas na solicitação da planilha

--- src/credsGoogleSheetsApi.py
+++ src/credsGoogleSheetsApi.py
@@ -3,30 +3,23 @@ import os.path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-SAMPLE_SPREADSHEET_ID = "1dkKQhT6rHWm8Xvz-qN9B9zm3kspugkUYceU8qACDJHI"
-SAMPLE_RANGE_NAME = "DB1!A1:F82"
 
 
 class ConnectGoogleSheetsApi():
     def __init__(self):
         self.creds = ""
+        self.SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
     def connect(self):
         self.creds = None
         if os.path.exists("token.json"):
-            self.creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+            self.creds = Credentials.from_authorized_user_file("token.json", self.SCOPES)
 
         if not self.creds or not self.creds.valid:
             if self.creds and self.creds.expired and self.creds.refresh_token:
                 self.creds.refresh(Request())
             else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    "credentials.json", SCOPES
-                )
+                flow = InstalledAppFlow.from_client_secrets_file("credentials.json", self.SCOPES)
                 self.creds = flow.run_local_server(port=0)
                 
             with open("token.json", "w") as token:
--- src/main.py
+++ src/main.py
@@ -1,5 +1,26 @@
 from credsGoogleSheetsApi import ConnectGoogleSheetsApi
 
-conn = ConnectGoogleSheetsApi()
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
+conn = ConnectGoogleSheetsApi()
 conn.connect()
+
+print("------CREDENCIAL")
+print(conn.creds)
+
+def getSheetsValues(conn):
+
+    try:
+        service = build('sheets', 'v4', credentials=conn.creds)
+        sheet = service.spreadsheets()
+        result = (sheet.values().get(spreadsheetsId="1dkKQhT6rHWm8Xvz-qN9B9zm3kspugkUYceU8qACDJHI", range="DB1!A1:F82").execute())
+        return result
+
+    except HttpError as err:
+        print(err)
+
+result = getSheetsValues(conn)
+
+print("---RESULTADO")
+print(result)
